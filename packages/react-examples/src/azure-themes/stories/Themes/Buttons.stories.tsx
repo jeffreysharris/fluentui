e8e4ffea56700c02f8f95291960bf8b6b31d2d49
jeffreysharris <j@ffrey.co>
2022-02-01 15:22:17 -0800

    Fixed contextualmenu overlap bug

---
+++ packages/react-examples/src/azure-themes/stories/Themes/Buttons.stories.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import { TextField, Stack, Checkbox, SearchBox, Link, Label, Text, ThemeProvider } from '@fluentui/react';
+import {
+  AzureThemeLight,
+  AzureThemeDark,
+  AzureThemeHighContrastLight,
+  AzureThemeHighContrastDark,
+} from '@fluentui/azure-themes';
+import { DefaultButton, CompoundButton, PrimaryButton } from '@fluentui/react/lib/Button';
+import { ButtonCommandBarExample } from '../components/commandBarButton.stories';
+import { ButtonSplitExample } from '../components/splitButton.stories';
+import { ButtonIconExample } from '../components/iconButton.stories';
+import { ButtonIconWithTooltipExample } from '../components/iconTooltip.stories';
+import { ButtonContextualMenuExample } from '../components/contextualMenu.stories';
+import { ButtonActionExample } from '../components/actionButton.stories';
+import { ButtonToggleExample } from '../components/buttonToggle.stories';
+import { CalloutBasicExample } from '../components/callout.stories';
+import { ActivityItemBasicExample } from '../components/activityitem.stories';
+import { ChoiceGroupBasicExample } from '../components/choicegroup.stories';
+import { ToggleBasicExample } from '../components/toggle.stories';
+import { ColorPickerBasicExample } from '../components/colorpicker.stories';
+import { ComboBoxBasicExample } from '../components/comboBox.stories';
+import { ContextualMenuDefaultExample } from '../components/ContextMenu.stories';
+import { DropdownBasicExample } from '../components/dropdown.stories';
+import { CommandBarBasicExample } from '../components/commandBar.stories';
+import { TagPickerBasicExample } from '../components/tags.stories';
+import { DetailsListCompactExample } from '../components/detailsList.stories';
+import { DatePickerBoundedExample } from '../components/dateBoundary.stories';
+import { PivotBasicExample } from '../components/Pivots.stories';
+import { TeachingBubbleBasicExample } from '../components/TeachingBubble.stories';
+import { MessageBarBasicExample } from '../components/messageBar.stories';
+import { TooltipBasicExample } from '../components/tooltip.stories';
+import { SliderBasicExample } from '../components/slider.stories';
+
+const Example = () => (
+  <Stack gap={8} horizontalAlign="center" style={{ maxWidth: 1000 }}>
+    <Stack gap={8} padding={32} horizontalAlign="center">
+      <Text>13px body text</Text>
+      <Label>MessageBar / InfoBox</Label>
+      <MessageBarBasicExample />
+    </Stack>
+    <Stack
+      gap={8}
+      horizontalAlign="center"
+      style={{ width: '100%', paddingTop: '64px', paddingBottom: '88px' }}
+      id="TeachingBubble"
+    >
+      <Label>TeachingBubble</Label>
+      <TeachingBubbleBasicExample />
+    </Stack>
+    <Stack
+      gap={8}
+      padding={64}
+      horizontalAlign="center"
+      style={{ width: '100%', paddingTop: '64px', paddingBottom: '88px' }}
+      id="Pivots"
+    >
+      <Label>Pivots</Label>
+      <PivotBasicExample />
+    </Stack>
+    <Stack gap={8} horizontalAlign="center">
+      <Label>Buttons</Label>
+      <DefaultButton id="DefaultButton" text="DefaultButton" />
+      <PrimaryButton id="PrimaryButton" text="PrimaryButton" />
+      <CompoundButton id="CompoundButton-1" primary text="CompoundButton" />
+      <CompoundButton id="CompoundButton-2" secondaryText="secondary text." text="CompoundButton" />
+      <DefaultButton id="DefaultAsPrimary" primary={true} text="Default button as primary" />
+      <DefaultButton id="DefaultAsPrimaryDisabled" primary={true} disabled={true} text="Default w/ primary disabled" />
+      <Label>Disabled Buttons</Label>
+      <DefaultButton id="DefaultDisabled" disabled text="DefaultButton disabled" />
+      <PrimaryButton id="PrimaryDisabled" disabled text="PrimaryButton disabled" />
+      <CompoundButton id="CompoundDisabled" disabled primary text="CompoundButton primary disabled" />
+      <Label disabled>I am a disabled label</Label>
+      <Label>Icon Buttons</Label>
+      <ButtonIconExample checked={false} />
+      <ButtonCommandBarExample />
+      <ButtonIconWithTooltipExample />
+      <Stack gap={8} padding={64} horizontalAlign="center" style={{ minHeight: '480px' }} id="ContextualMenu">
+        <ButtonContextualMenuExample />
+      </Stack>
+      <ButtonActionExample />
+
+      <Label>Toggle button</Label>
+      <ButtonToggleExample />
+      <ButtonSplitExample checked={false} />
+      <CalloutBasicExample />
+      <DefaultButton text="WIP: default button > primary" primary />
+      <DefaultButton text="WIP: Primary button" primary />
+    </Stack>
+  </Stack>
+);
+
+export const Light = () => (
+  <ThemeProvider theme={AzureThemeLight} applyTo="body">
+    <Example />
+  </ThemeProvider>
+);
+
+export const Dark = () => (
+  <ThemeProvider theme={AzureThemeDark} applyTo="body">
+    <Example />
+  </ThemeProvider>
+);
+
+export const HighContrastLight = () => (
+  <ThemeProvider theme={AzureThemeHighContrastLight} applyTo="body">
+    <Example />
+  </ThemeProvider>
+);
+
+export const HighContrastDark = () => (
+  <ThemeProvider theme={AzureThemeHighContrastDark} applyTo="body">
+    <Example />
+  </ThemeProvider>
+);
--- packages/react-examples/src/azure-themes/stories/components/contextualMenu.stories.tsx
+++ packages/react-examples/src/azure-themes/stories/components/contextualMenu.stories.tsx
@@ -1,31 +1,21 @@
 import * as React from 'react';
-import { ContextualMenu, IContextualMenuProps, IIconProps } from '@fluentui/react';
+import {
+  ContextualMenu,
+  Customizer,
+  IContextualMenuProps,
+  IIconProps,
+  ILayerProps,
+  LayerHost,
+  mergeStyleSets,
+} from '@fluentui/react';
 import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
+import { useId } from '@fluentui/react-hooks';
 
 export interface IButtonExampleProps {
   // These are set based on the toggles shown above the examples (not needed in real code)
   disabled?: boolean;
   checked?: boolean;
 }
-
-const menuProps: IContextualMenuProps = {
-  // For example: disable dismiss if shift key is held down while dismissing
-  items: [
-    {
-      key: 'emailMessage',
-      text: 'Email message',
-      iconProps: { iconName: 'Mail' },
-    },
-    {
-      key: 'calendarEvent',
-      text: 'Calendar event',
-      iconProps: { iconName: 'Calendar' },
-    },
-  ],
-  directionalHintFixed: true,
-};
-
-const addIcon: IIconProps = { iconName: 'Add' };
 
 export const ButtonContextualMenuExample: React.FunctionComponent<IButtonExampleProps> = (
   props: IButtonExampleProps,
@@ -34,14 +24,58 @@ export const ButtonContextualMenuExample: React.FunctionComponent<IButtonExample
 
   let menuButton: React.RefObject<HTMLInputElement> | null;
 
+  const _calloutTarget = React.createRef<HTMLDivElement>();
+
+  const layerHostId = useId('contextualMenuLayerHost');
+
+  const layerProps: ILayerProps = { hostId: layerHostId };
+
+  const menuProps: IContextualMenuProps = {
+    items: [
+      {
+        key: 'emailMessage',
+        text: 'Email message',
+        iconProps: { iconName: 'Mail' },
+      },
+      {
+        key: 'calendarEvent',
+        text: 'Calendar event',
+        iconProps: { iconName: 'Calendar' },
+      },
+    ],
+    directionalHintFixed: true,
+    id: 'ContextualMenuTest',
+    alignTargetEdge: true,
+    hidden: false,
+    target: _calloutTarget,
+    calloutProps: {
+      preventDismissOnEvent: () => true,
+    },
+  };
+
+  const addIcon: IIconProps = { iconName: 'Add' };
+
+  const styles = mergeStyleSets({
+    layerHost: {
+      position: 'relative',
+      height: 400,
+      width: 1000,
+      // overflow: 'hidden',
+      border: '1px solid #ccc',
+    },
+    contextualMenu: {
+      opacity: 1,
+      filter: 'opacity(1)',
+    },
+  });
+
   const buttonProps = {
     text: 'New item',
     iconProps: addIcon,
     menuProps: menuProps,
     // Optional callback to customize menu rendering
-    menuAs: _getMenu,
+    // menuAs: _getMenu,
     // Optional callback to do other actions (besides opening the menu) on click
-    onMenuClick: _onMenuClick,
     // By default, the ContextualMenu is re-created each time it's shown and destroyed when closed.
     // Uncomment the next line to hide the ContextualMenu but persist it in the DOM instead.
     persistMenu: true,
@@ -61,54 +95,52 @@ export const ButtonContextualMenuExample: React.FunctionComponent<IButtonExample
   });
 
   return (
-    <>
-      <DefaultButton
-        id="ContextualMenuButton"
-        componentRef={(node: any) => {
-          console.log('attaching node as componentRef');
-          if (node?._buttonElement) {
-            menuButton = node._buttonElement;
-          }
-        }}
-        onClick={buttonProps.onMenuClick}
-        {...buttonProps}
-      />
-      <PrimaryButton {...buttonProps} />
-      <ContextualMenu
-        id="ContextualMenu-1"
-        // onDismiss={(ev: any) => {
-        //   ev.preventDefault();
-        //   ev.stopPropagation();
-        //   _getMenu(menuProps)
-        // }}
-        target="#ContextualMenuButton"
-        {...menuProps}
-      />
-    </>
+    // <div id="ContextualMenuExample">
+    //   <DefaultButton
+    //     id="ContextualMenuButton"
+    //     componentRef={(node: any) => {
+    //       console.log('attaching node as componentRef');
+    //       if (node?._buttonElement) {
+    //         menuButton = node._buttonElement;
+    //       }
+    //     }}
+    //     onClick={buttonProps.onMenuClick}
+    //     {...buttonProps}
+    //   />
+    //   <PrimaryButton {...buttonProps} />
+    //   <ContextualMenu
+    //     id="ContextualMenu-1"
+    //     onDismiss={(ev: any) => {
+    //       ev.preventDefault();
+    //       ev.stopPropagation();
+    //       _getMenu(menuProps);
+    //     }}
+    //     target="#ContextualMenu"
+    //     {...menuProps}
+    //   />
+    // </div>
+    <div>
+      <Customizer scopedSettings={{ Layer: { ...layerProps } }}>
+        <LayerHost id={layerHostId} className={styles.layerHost}>
+          <ContextualMenu {...menuProps} id="ContextualMenuExample" />
+          <div className="targetClass" ref={_calloutTarget} />
+        </LayerHost>
+      </Customizer>
+    </div>
   );
 };
 
-function _menuOpened(_props: IContextualMenuProps): void {
-  console.log('_menuOpened');
-}
-
 function _getMenu(props: IContextualMenuProps): JSX.Element {
-  console.log('_getMenu');
   // Customize contextual menu with menuAs
   return (
     <ContextualMenu
-      id="ContextualMenu-2"
-      onMenuOpened={_menuOpened}
+      id="ContextualMenu-"
       onDismiss={(ev: any) => {
         ev.preventDefault();
         ev.stopPropagation();
-        _getMenu(menuProps);
+        _getMenu(props);
       }}
       {...props}
     />
   );
-}
-
-function _onMenuClick(_ev?: React.SyntheticEvent<any>): void {
-  console.log('_onMenuClick');
 }
